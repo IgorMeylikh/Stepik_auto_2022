d274be8b006efa3a4472382ac9dc23a521330fe8
Мейлих Игорь <igor.meilih@osinit.com>
2022-10-19 16:19:37 +0300

    Продолжаю обучение автотестированию

--- dev_in_auto_files/Lesson_3_2_12_unittest.py
+++ dev_in_auto_files/Lesson_3_2_12_unittest.py
@@ -1,37 +1,12 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-import time
-import math
+import unittest
 
-def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
-try:
-    link = 'http://suninjuly.github.io/explicit_wait2.html'
-    browser = webdriver.Chrome()
-    browser.get(link)
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
 
-    price = WebDriverWait(browser, 12).until(
-        EC.text_to_be_present_in_element((By.ID, 'price'), '100')
-    )
-
-    button = browser.find_element(By.ID, 'book')
-    button.click()
-    time.sleep(1)
-
-    x_var = browser.find_element(By.ID, 'input_value')
-    x_var = x_var.text
-
-    answer = browser.find_element(By.ID, 'answer')
-    answer.send_keys(calc(x_var))
-
-    button_submit = browser.find_element(By.ID, 'solve')
-    button_submit.click()
+    def test_abs2(self):
+        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
 
 
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(5)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+if __name__ == "__main__":
+    unittest.main()
--- dev_in_auto_files/Lesson_3_2_13_unittest.py
+++ dev_in_auto_files/Lesson_3_2_13_unittest.py
@@ -1,12 +1,74 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import unittest
-
-class TestAbs(unittest.TestCase):
-    def test_abs1(self):
-        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
-
-    def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+import time
 
 
-if __name__ == "__main__":
-    unittest.main()
+
+try:
+    browser = webdriver.Chrome()
+
+    class TestRegistration(unittest.TestCase):
+        def test_registration_1(self):
+            link_1 = "http://suninjuly.github.io/registration1.html"
+            browser.get(link_1)
+
+            # Ваш код, который заполняет обязательные поля
+            first_field = browser.find_element(By.XPATH, "//input[@placeholder = 'Input your first name']")
+            first_field.send_keys("First name")
+            second_field = browser.find_element(By.XPATH, "//input[@placeholder = 'Input your last name']")
+            second_field.send_keys("Last name")
+            third_field = browser.find_element(By.XPATH, "//input[@placeholder = 'Input your email']")
+            third_field.send_keys("mail@mail.ru")
+
+            # Отправляем заполненную форму
+            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+            self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "Error_1")
+
+        def test_registration_2(self):
+            link_2 = "http://suninjuly.github.io/registration2.html"
+            browser.get(link_2)
+            # Ваш код, который заполняет обязательные поля
+            first_field = browser.find_element(By.XPATH, "//input[@placeholder = 'Input your name']")
+            first_field.send_keys("First name")
+
+            second_field = browser.find_element(By.XPATH, "//input[@placeholder = 'Input your last name']")
+            second_field.send_keys("Last name")
+
+            third_field = browser.find_element(By.XPATH, "//input[@placeholder = 'Input your email']")
+            third_field.send_keys("mail@mail.ru")
+
+            # Отправляем заполненную форму
+            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+            self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "Error_2")
+
+
+    if __name__ == "__main__":
+        unittest.main()
+finally:
+    time.sleep(5)
+    browser.quit()
+
+
+
+
